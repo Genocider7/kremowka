9e00b203727f67ef608be48be2d9cba727f311f1
Genocider7 <blazerwarrior@gmail.com>
2023-03-03 12:37:33 +0100

    Fixed changing presence

--- bot.py
+++ bot.py
@@ -136,7 +136,7 @@ async def on_ready():
     log('Connected to discord servers!')
     log('logged in as ' + client.user.name)
     log('id ' + str(client.user.id))
-    activity = discord.CustomActivity('{prefix}help'.format(config['prefix']))
+    activity = discord.CustomActivity('{prefix}help'.format(prefix=config['prefix']))
     client.change_presence(status=discord.Status.online, activity=activity)
     scheduler = AsyncIOScheduler(timezone='Europe/Warsaw')
     scheduler.add_job(stop_receiving_memes, CronTrigger(hour=21, minute=35, second=0))
